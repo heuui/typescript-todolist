14438840084846bb30363daae8df8313724de91f
heuui <henni.haavistola@gmail.com>
2022-12-03 15:29:44 +0200

    date lisätty ja tyyliittelyä

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
+import { format } from "path"
 import { v4 as uuidV4 } from "uuid"
 
 type Todo = { 
   id: string,
   description: string
   completed: boolean
-  creation_date: Date
+  date: string
 }
 
 // määritellään elementtien tyypit
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new-todo-form") as HTMLFormElement | null
 const inputDesc = document.querySelector<HTMLInputElement>("#new-todo-desc")
+const inputDate = document.querySelector<HTMLInputElement>("#new-todo-date")
 
 const todos: Todo[] = loadTodos()
 todos.forEach(addListItem)
@@ -19,34 +21,38 @@ form?.addEventListener("submit", e => {
   e.preventDefault()
 
   if (inputDesc?.value == "" || inputDesc?.value == null) return // undefined
+  if (inputDate?.value == "" || inputDate?.value == null) return
 
   // todo olion määritys
   const newTodo: Todo = {
     id: uuidV4(), // luo todolle id:n
     description: inputDesc.value,
     completed : false,
-    creation_date: new Date(),
+    date: inputDate.value
   }
   todos.push(newTodo) // tallennetaan uusi todo listaan.
 
   addListItem(newTodo)
   inputDesc.value = ""
+  inputDate.value = ""
 
 })
 
 function addListItem(todo: Todo) {  // add new todo which type is Todo
-  const item = document.createElement("li")
+  const li = document.createElement("li")
+  const pvm = document.createElement('h4')
   const label = document.createElement("label")
   const checkbox = document.createElement("input")
   checkbox.addEventListener("change", () => {
     todo.completed = checkbox.checked 
     saveTodos()
   })
+
   checkbox.type = "checkbox"
   checkbox.checked = todo.completed
-  label.append(checkbox, todo.description)
-  item.append(label)
-  list?.append(item)
+  label.append(todo.date, " - ", todo.description, "  ", checkbox)
+  li.append(pvm, label)
+  list?.append(li)
 }
 
 function saveTodos() {  // tallentaa todot localstorageen
